568d01cbb389edc4b70216a779c9989ae2a1fa33
AleexxG <alexanderg.sln@gmail.com>
2024-06-09 11:09:01 +0200

    Alpine counter

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <h1>New Post</h1>
+    <h2>New Post</h2>
 
     <form wire:submit="save">
         <label for="title">Title</label>
@@ -18,4 +18,15 @@
         <br>
         <button type="submit">Create</button>
     </form>
+
+    <hr>
+
+    <div>
+        <h3>Alpine Counter</h3>
+        <div x-data="{ count: 0 }">
+            <button x-on:click="count--">-</button>
+            <span x-text="count"></span>
+            <button x-on:click="count++">+</button>
+        </div>
+    </div>
 </div>
